603c5c3e033c655e3a4614eecd689cb31aa22817
Bolivar Delgado <bolivar.delgado@gmail.com>
2025-06-13 19:46:49 -0500

    chore: category-dropdown

--- src/modules/home/ui/components/category-dropdown.tsx
+++ src/modules/home/ui/components/category-dropdown.tsx
@@ -3,7 +3,6 @@
 import { Button } from "@/components/ui/button"
 import { cn } from "@/lib/utils"
 import { useRef, useState } from "react"
-import { useDropdownPosition } from "../../hooks/use-dropdown-position"
 import { SubcategoryMenu } from "./subcategory-menu"
 import Link from "next/link"
 import { CategoriesGetManyOutput } from "@/modules/categories/types"
@@ -21,7 +20,6 @@ export const CategoryDropDown = ({
 }: Props) => {
   const [isOpen, setIsOpen] = useState(false)
   const dropdownRef = useRef<HTMLDivElement>(null)
-  const { getDropdownPosition } = useDropdownPosition(dropdownRef)
 
   const onMouseEnter = () => {
     if (category.subcategories) {
